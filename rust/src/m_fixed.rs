56e413ecaa8e15b6d009699a6e9c548386b9c3c2
mgr-inz-rafal <rchabowski@gmail.com>
2020-05-03 11:59:03 +0200

    Use "function_name" crate for logging

--- rust/src/m_fixed.rs
+++ rust/src/m_fixed.rs
@@ -1,22 +1,26 @@
 use std::convert::TryInto;
 use std::i32;
 
+use ::function_name::named;
+
 type fixed_t = i32;
 
 const FRACBITS: fixed_t = 16;
 
+#[named]
 #[no_mangle]
 pub extern "C" fn FixedMul(a: fixed_t, b: fixed_t) -> fixed_t {
     let value = (a as i64 * b as i64) >> FRACBITS;
     match value.try_into() {
         Ok(value) => value,
         Err(_) => {
-            println!("Rust: FixedMul would panic!");
+            println!("Rust: {} would panic!", function_name!());
             value as fixed_t
         }
     }
 }
 
+#[named]
 #[no_mangle]
 pub extern "C" fn FixedDiv(a: fixed_t, b: fixed_t) -> fixed_t {
     if (a.abs() >> 14) >= b.abs() {
@@ -31,7 +35,7 @@ pub extern "C" fn FixedDiv(a: fixed_t, b: fixed_t) -> fixed_t {
     match value.try_into() {
         Ok(value) => value,
         Err(_) => {
-            println!("Rust: FixedDiv would panic!");
+            println!("Rust: {} would panic!", function_name!());
             value as fixed_t
         }
     }
